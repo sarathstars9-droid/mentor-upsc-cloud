d14b86d7b432a5f37f13be12e71d17aff32c6943
Sarath Vaddiparthi <sarath.stars9@gmail.com>
2026-03-10 18:08:26 +0530

    fix backend url

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 export const SCRIPT_URL =
   "https://script.google.com/macros/s/AKfycbyOaohtPtFG7wPZw1Kd4gl7D8pbW1cka19wSYKyjlnu9fUUJZyEUDbxyAoSfJMhidhs6g/exec";
 
-export const API_BASE = "https://api.mentorupsc.in";
+export const BACKEND_URL = "https://api.mentorupsc.in";
