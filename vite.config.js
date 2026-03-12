1b8872d3b03a997fbe0929f5efbe9edacc39cc36
Sarath Vaddiparthi <sarath.stars9@gmail.com>
2026-03-12 23:16:03 +0530

    fix vercel routing

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/",   // ← ADD THIS
+
   plugins: [react()],
+
   server: {
     host: "0.0.0.0",
     port: 5173,
+
     proxy: {
       "/api/script": {
         target:
