16ef4c9ed4a989728ef460d68e5af532fa591ba2
Sarath Vaddiparthi <sarath.stars9@gmail.com>
2026-05-19 11:50:57 +0530

    Improve mains answer writing and MentorOS theme support

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
 import "./index.css";
 import "./styles/mentoros-theme.css";
 import "./styles/mentoros-layout.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
